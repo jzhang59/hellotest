3ecfb66efa93f37b76869872927bd0e7c302e0ee
jzhang59 <75959515+jzhang59@users.noreply.github.com>
2021-01-17 14:38:15 -0500

    Update index.php
    changed demo to test.

--- index.php
+++ index.php
@@ -48,7 +48,7 @@ img {
 <div id="body-wrapper">
 <?php 
 echo "
-<h1>Image Library with " . $storage_type . " storage backend Demo.</h1>
+<h1>Image Library with " . $storage_type . " storage backend test.</h1>
 "
 ?>
 <div class="wrapper" id="upload-form">
